1d8169e9e00469ba487775930b0568e7b6385445
MaxAlv320 <st4086@utr.edu.mx>
2025-12-03 20:21:19 -0600

    Arreglo MainLayout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,11 +1,28 @@
 import TopBar from "../Component/TopBar";
-import { Outlet } from "react-router-dom";
+import Navbar from "../Component/Navbar";
+import { Outlet, useLocation } from "react-router-dom";
 
 export default function MainLayout() {
+  const location = useLocation();
+
+  const adminRoutes = {
+    "/adminhome": "Admin Home",
+    "/inventary": "Inventory",
+    "/menumanagement": "Menu Management",
+    "/burgeredit": "Edit Burger",
+    "/ordermanagement": "Order Management",
+  };
+
+  const currentPath = location.pathname;
+  const adminTitle = adminRoutes[currentPath];
+
   return (
     <>
-      <TopBar />
-      <Outlet />
+      {!adminTitle && <TopBar />}
+      {adminTitle && <Navbar title={adminTitle} />}
+      <div style={{ marginTop: adminTitle ? "120px" : "0px" }}>
+        <Outlet />
+      </div>
     </>
   );
 }
